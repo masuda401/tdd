a17483c7a558ec51e0d2850994b1e62a1a2370b6
shinichi <shinichi@10.27.44.14>
2018-02-04 19:58:54 +0900

    initial commit.

---
+++ src/main/java/com/example/Dollar.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Dollar {
+	int amount;
+
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	void times(int multiplier) {
+		amount *= multiplier;
+	}
+	
+}
