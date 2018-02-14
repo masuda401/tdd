b671d1b623f9ca521ee9debb6aba99754e5dc59d
shinichi <shinichi@192.168.12.16>
2018-02-14 22:39:20 +0900

    implemented "Interesting Times"(Chapter 10).

--- src/main/java/com/example/MoneyTest.java
+++ src/main/java/com/example/MoneyTest.java
@@ -34,5 +34,10 @@ class MoneyTest {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
+	
+	@Test
+	public void testDifferentClassEquality() {
+		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+	}
 
 }
